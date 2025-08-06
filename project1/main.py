3ca6c7bd0d0ad2e6c46e7714fd4c2c4acfa03167
Costa <cody.costa@kla.com>
2025-08-06 16:38:46 -0700

    renamed project 1 source files

--- project1/main.py
+++ project1/main.py
@@ -9,7 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-from crater import crater_centroids
 
 # load image into grayscale form
 img = 'defects.png'
@@ -24,19 +23,36 @@ gray_enhanced = clahe.apply(gray)
 blurred = cv2.GaussianBlur(gray_enhanced, (7, 7), 0)
 
 
+''' LIGHT AREA BUMP DEFECTS '''
 # playing around with these values to tune results
-C = -6
+C = -5
+BLOCK_SIZE = 27
+
+# binarize image
+bump_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, BLOCK_SIZE, C)
+
+# find contours
+bump_contours, _ = cv2.findContours(bump_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# print(contours)
+
+
+''' DARK AREA CRATER DEFECTS '''
+# playing around with these values to tune results
+C = 16
 BLOCK_SIZE = 25
 
 # binarize image
-thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, BLOCK_SIZE, C)
+crater_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, BLOCK_SIZE, C)
 
 # find contours
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+crater_contours, _ = cv2.findContours(crater_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 # print(contours)
+
 
 # record centroids of each defect
 centroids = []
+
+contours = bump_contours + crater_contours
 
 # plot defects on original image
 for contour in contours:
@@ -47,32 +63,32 @@ for contour in contours:
         cy = int(M['m01'] / M['m00'])
 
         # ignore large dark areas
-        if cy < 16 or cx > 887:
-            continue
+        # if cy < 16 or cx > 887:
+        #     continue
 
         centroids.append((cx, cy))
 
-        cv2.circle(blurred, (cx, cy), 5, (0, 255, 0), 2)
+        # cv2.circle(blurred, (cx, cy), 5, (0, 255, 0), 2)
         
-    # cv2.drawContours(blurred, [contour], -1, (0, 0, 255), 1)
+    cv2.drawContours(blurred, [contour], -1, (0, 0, 255), 2)
 
-for item in crater_centroids:
-    cx = item[0]
-    cy = item[1]
+# for item in crater_centroids:
+#     cx = item[0]
+#     cy = item[1]
 
-    cv2.circle(blurred, (cx, cy), 5, (0, 255, 0), 2)
+#     cv2.circle(blurred, (cx, cy), 5, (0, 255, 0), 2)
 
-centroids += crater_centroids
+# centroids += crater_centroids
     
 cv2.imwrite('defects_labeled_improved.jpg', blurred)
 
 cv2.imshow('GE', gray_enhanced)
 cv2.imshow('blurred', blurred)
-cv2.imshow('binary', thresh)
+# cv2.imshow('binary', thresh)
 # cv2.imshow('defects', image)
 
 # print defect count
-print(f'defect count = {len(centroids)}')
+print(f'defect count = {len(contours)}')
 
 
 
